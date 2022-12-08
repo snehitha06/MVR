97fab4a084a281ea7d577f2eef219b4b35f93a2c
snehitha06 <69567782+snehitha06@users.noreply.github.com>
2022-12-08 11:03:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./containers/Home";
+import Details from "./containers/Details";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        {/* Details Route */}
+        <Route path="/city">
+          <Details />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
